3bdbd0620084e6dc4eab80c265cc4c0118a52e70
Diego Garcia Cordeiro Souza <diego00alfa@gmail.com>
2026-04-25 00:34:36 -0300

    feat: theme toggling system
    Implemented a theme toggling system. Organized styles into CSS files. Added FOUC prevention on inlined
    script at head. Added a few starter components.

---
+++ src/scripts/theme.ts
@@ -0,0 +1,43 @@
+const THEMES = Object.freeze([
+  "light", "dark", "retro-light", "retro-dark"
+]);
+
+/**
+ * Retrieves the browser's preferred color scheme.
+ * @returns Browser's preferred color scheme.
+ */
+export function browserPreference(): string {
+  return window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+}
+
+/**
+ * Retrieves the theme set on localStorage. Defaults to "an empty string.
+ * @returns Theme set on local storage.
+ */
+export function getLocalStorageTheme(): string {
+  const localStorageTheme = window.localStorage?.getItem("theme") ?? "";
+
+    if (THEMES.includes(localStorageTheme)) {
+      return localStorageTheme;
+    }
+
+    return "";
+}
+
+/**
+ * Initial page load theme resolver. Defaults to "light".
+ * @returns Theme to be set on initial page load.
+ */
+export function initialTheme(): string {
+  return getLocalStorageTheme() || browserPreference();
+}
+
+/**
+ * Sets a new theme.
+ */
+export function setTheme(theme: string): void {
+  document.documentElement.setAttribute("data-theme", theme);
+  window.localStorage?.setItem("theme", theme);
+}
