9640246b5c76e08d70d0c2a36d23032cbffd13eb
Diego Garcia Cordeiro Souza <diego00alfa@gmail.com>
2026-08-09 22:43:04 -0300

    chore: small stylelint adjustment
    Turned the "custom-property-empty-line-before" off in stylelint.config.js. Also removed
    the "stylelint-config-standard" nested within the overrides block, since its already
    available from the top-level import.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,12 @@
 export default {
   "extends": ["stylelint-config-standard"],
+  "rules": {
+    "custom-property-empty-line-before": null
+  },
   "overrides": [
     {
       files: ["*.astro", "**/*.astro"],
-      extends: ["stylelint-config-standard", "stylelint-config-html/astro"],
+      extends: ["stylelint-config-html/astro"],
       rules: {
         "selector-pseudo-class-no-unknown": [
           true,
